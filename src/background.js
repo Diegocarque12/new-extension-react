278380000b88ca6bc8a8c1145b40a8a94dea7e95
Diego Carvajal <49083313+Diegocarque12@users.noreply.github.com>
2024-06-26 17:34:09 -0600

    Add files via upload

--- src/background.js
+++ src/background.js
@@ -1,3 +1,19 @@
+chrome.action.onClicked.addListener(() => {
+  chrome.windows.getCurrent((currentWindow) => {
+    const top = currentWindow.top;
+    const left = currentWindow.left + currentWindow.width - 400; // Ajusta el ancho de la ventana según sea necesario
+
+    chrome.windows.create({
+      url: chrome.runtime.getURL("index.html"),
+      type: "popup",
+      width: 500,
+      height: 400,
+      top: top,
+      left: left
+    });
+  });
+});
+
 chrome.runtime.onInstalled.addListener(() => {
   console.log('La extensión está instalada.');
 });
